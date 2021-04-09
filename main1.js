a5b0ff4e16419afeee46110175b98919507ef65a
Lala <--replace>
2021-04-10 02:10:20 +0400

    first commit

---
+++ main1.js
@@ -0,0 +1,12 @@
+function myFunction(){
+if(document.getElementById("icons").style.display == "none"){
+    document.getElementById("icons").style.display = "block";
+  
+} else{
+    document.getElementById("icons").style.display = "none";
+    
+}
+
+}
+
+
